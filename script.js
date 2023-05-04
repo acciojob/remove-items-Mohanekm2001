55948cf75df13d20f1867508767f89ff0172eff8
Mohanekm2001 <mohan2001ekm@gmail.com>
2023-05-04 13:09:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,26 @@
-//your JS code here. If required.
-//your JS code here. If required.
-function removeColor() {
-  const select = document.getElementById("colorSelect");
-  const selectedOption = select.options[select.selectedIndex];
-  select.removeChild(selectedOption);
+let sel=document.getElementById("colorSelect")
+let btn=document.getElementsByTagName("input")[0];
+
+// 
+ btn.addEventListener("click",remove);
+
+
+function remove(){
+
+    let item=getSelected();
+    
+    for(let i=0;i<sel.length;i++){
+        let child=sel[i];
+        if(child.value==item){
+            sel.removeChild(child);
+            break;
+        }
+    }
 }
+
+let getSelected=function(){
+	if(sel.length==0) return;
+    let item=document.getElementById("colorSelect").value;
+    return item;
+}
+let item=sel.addEventListener("change", getSelected);
